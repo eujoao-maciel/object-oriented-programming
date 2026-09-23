121cb0bf22effc670c165928d23f70561d4ae05a
notebook da lesli <joao.eudes13jem@gmail.com>
2026-09-23 17:04:05 -0300

    practice java constructors

--- classes/Aluno.java
+++ classes/Aluno.java
@@ -1,17 +1,24 @@
+import java.util.Random;
+
 public class Aluno {
   private String nome;
+  private int idade;
+  private double codigo_identificador;
+  private Random aleatorio;
 
-  public void inserirNome(String nome) {
+  public Aluno(String nome, int idade) {
+    aleatorio = new Random();
     this.nome = nome;
+    this.idade = idade;
+    this.codigo_identificador = aleatorio.nextDouble();
   }
 
-  public String recuperarNome() {
-    return nome;
+  public String recuperarDados() {
+    return "Nome: " + nome + ", Idade: " + idade; 
   }
-
+  
   public static void main(String args[]) {
-    Aluno aluno1 = new Aluno();
-    aluno1.inserirNome("João");
-    System.out.println("Aluno: "+aluno1.recuperarNome());
+    Aluno aluno1 = new Aluno("João", 24);
+    System.out.println(aluno1.recuperarDados());
   }
 }
